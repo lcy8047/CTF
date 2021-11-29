6156156e59055b67507a854d501b9e111eb89211
lcqff <afteryear199@naver.com>
2021-11-29 18:10:31 +0900

    Dreamhack Baskin Game

--- lcqff/HackCTF_Misc_dalalan/hackctf_Misc_Dalalan.py
+++ lcqff/HackCTF_Misc_dalalan/hackctf_Misc_Dalalan.py
@@ -1,18 +1,20 @@
 #Hack-ctf misk 달라란 침공
+
+
 from pwn import *
 r = remote("ctf.j0n9hyun.xyz",9003)
 d = r.recv()
-print(d.decode('utf-8'))
+print(d.decode('utf-8')) #우리 악당들이 모여 달라란을 차지하자!
 d = r.recv()
-print(d.decode('utf-8'))
-r.sendline(b'\x31') #그냥 '1'보내도 됨
+print(d.decode('utf-8')) # 문제 1) ,2), 3)
+r.sendline(b'\x31') #'1' 입력
 
 for i in range(20):
-    d = r.recvuntil(b'\n') #:까지
+    d = r.recvuntil(b'\n') #뭐였는지 기억안남
     print(d.decode('utf-8'))
 
 
-    d = r.recvline() #공식
+    d = r.recvline() #공식받아오기
     formula = d.decode('utf-8');
     answer = eval(formula);
     r.sendline(str(answer)); #string으로 보내주기
@@ -27,7 +29,7 @@ for i in range(20):
 
 d = r.recvuntil('input )')
 print(d.decode('utf-8'))
-r.sendline('2') #그냥 '1'보내도 됨
+r.sendline('2')
 
 for i in range(30):
     d = r.recvuntil(b'\n') #:까지
